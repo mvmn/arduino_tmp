9d9454cf63cddd42a7c66c3ed09e77309d71b0d5
Mykola Makhin <mvmn.inbox@gmail.com>
2016-04-23 16:26:36 +0300

    16x2 screen analog clock - shortened hour arrow a bit more.

--- RealTimeClockDisplaySensor/RealTimeClockDisplaySensor.ino
+++ RealTimeClockDisplaySensor/RealTimeClockDisplaySensor.ino
@@ -163,10 +163,10 @@ void updateDisplay() {
     drawArrow(2, 12, 9, 9, 6, 60, pha);
     }
     pha = tm.Hour;*/
-  drawArrow(0, 11, 8, 9, 6, 12 * 60, (tm.Hour % 12) * 60 + tm.Minute);
-  drawArrow(0, 10, 8, 9, 6, 12 * 60, (tm.Hour % 12) * 60 + tm.Minute);
-  drawArrow(0, 11, 9, 9, 6, 12 * 60, (tm.Hour % 12) * 60 + tm.Minute);
-  drawArrow(0, 10, 9, 9, 6, 12 * 60, (tm.Hour % 12) * 60 + tm.Minute);
+  drawArrow(0, 11, 8, 8, 5, 12 * 60, (tm.Hour % 12) * 60 + tm.Minute);
+  drawArrow(0, 10, 8, 8, 5, 12 * 60, (tm.Hour % 12) * 60 + tm.Minute);
+  drawArrow(0, 11, 9, 8, 5, 12 * 60, (tm.Hour % 12) * 60 + tm.Minute);
+  drawArrow(0, 10, 9, 8, 5, 12 * 60, (tm.Hour % 12) * 60 + tm.Minute);
 
   /*
     if (pma > -1 && pma != tm.Minute) {
